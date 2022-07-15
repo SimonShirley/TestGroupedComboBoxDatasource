610e5bb88de84479892c8428fb93437c2efa3a43
Simon Shirley <email@simonshirley.uk>
2022-07-15 12:06:39 +0100

    Upload Dummy project

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TestGroupedComboBoxDatasource
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboComboBox = new GroupedComboBox();
+            this.btnChangeDataSource = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboComboBox
+            // 
+            this.cboComboBox.DataSource = null;
+            this.cboComboBox.DisplayMember = "ItemName";
+            this.cboComboBox.FormattingEnabled = true;
+            this.cboComboBox.GroupMember = "GroupName";
+            this.cboComboBox.Location = new System.Drawing.Point(139, 82);
+            this.cboComboBox.Name = "cboComboBox";
+            this.cboComboBox.Size = new System.Drawing.Size(254, 21);
+            this.cboComboBox.TabIndex = 0;
+            this.cboComboBox.ValueMember = "ItemId";
+            // 
+            // btnChangeDataSource
+            // 
+            this.btnChangeDataSource.Location = new System.Drawing.Point(139, 120);
+            this.btnChangeDataSource.Name = "btnChangeDataSource";
+            this.btnChangeDataSource.Size = new System.Drawing.Size(254, 23);
+            this.btnChangeDataSource.TabIndex = 1;
+            this.btnChangeDataSource.Text = "Change DataSource";
+            this.btnChangeDataSource.UseVisualStyleBackColor = true;
+            this.btnChangeDataSource.Click += new System.EventHandler(this.btnChangeDataSource_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeDataSource);
+            this.Controls.Add(this.cboComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupedComboBox cboComboBox;
+        private System.Windows.Forms.Button btnChangeDataSource;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TestGroupedComboBoxDatasource
+{
+    public partial class Form1 : Form
+    {
+        private int itemCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var loadedDataSource = new List<DummyItem>();
+
+            Enumerable.Range(1, 3).ToList().ForEach(i =>
+            {
+                itemCounter++;
+                loadedDataSource.Add(new DummyItem { ItemId = Guid.NewGuid(), ItemName = string.Concat("Item ", itemCounter.ToString()), GroupName = "Main Group" });
+            });
+
+            cboComboBox.DataSource = loadedDataSource;
+        }
+
+        private void btnChangeDataSource_Click(object sender, EventArgs e)
+        {
+            var changedDataSource = new List<DummyItem>();
+
+            Enumerable.Range(1, 3).ToList().ForEach(i =>
+            {
+                itemCounter++;
+                changedDataSource.Add(new DummyItem { ItemId = Guid.NewGuid(), ItemName = string.Concat("Item ", itemCounter.ToString()), GroupName = "Main Group" });
+            });
+
+            cboComboBox.DataSource = changedDataSource;
+        }
+    }
+
+    /// <summary>
+    /// DummyItem class represents the Dto I'm using in my main project
+    /// </summary>
+    internal class DummyItem
+    {
+        public Guid ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string GroupName { get; set; }
+    }
+}
